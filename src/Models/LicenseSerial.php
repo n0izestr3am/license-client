d317141f2024bce668ad92d739ee123c3151b74b
n0izestr3am <n0izestr3am@gmail.com>
2023-07-27 16:34:51 +0700

    migrate models

--- src/Models/LicenseSerial.php
+++ src/Models/LicenseSerial.php
@@ -6,6 +6,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class LicenseSerial extends Model
 {
-	protected $table = 'license_serials';
+	protected $table = 'gtds_clients';
     protected $fillable = ['name','domain','phone_number','address','serial','expired','status'];	
 }
